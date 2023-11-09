c8ec2985a675214951c55a8076b3f8c770037822
Georgiana Marcu <92670549+georgianamarcu@users.noreply.github.com>
2023-11-09 10:13:37 +0100

    added upload option

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,110 @@
 import { Canvas } from "@react-three/fiber";
-import { Loader } from "@react-three/drei";
-import { Perf } from "r3f-perf";
-import Scene from "./components/Scene";
-import Overlay from "./components/Overlay";
-import { useState } from "react";
+import styled from "styled-components";
+import { Loader, useGLTF, Stage } from "@react-three/drei";
+import Controls from "./components/Controls";
+import {
+  createContext,
+  Suspense,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { TextureLoader, sRGBEncoding, RepeatWrapping } from "three";
+
+const AppContext = createContext({});
+
+const createImageUrl = (buffer, type) => {
+  const blob = new Blob([buffer], { type });
+  const urlCreator = window.URL || window.webkitURL;
+  const imageUrl = urlCreator.createObjectURL(blob);
+  return imageUrl;
+};
 
 function App() {
-  const [smallSofa, setSmallSofa] = useState(true);
+  const [textureUrl, setTextureUrl] = useState(null);
+  const getTextureFile = async (e) => {
+    const file = e.target.files[0];
+    const { type } = file;
+    const buffer = await file.arrayBuffer();
+    const imageUrl = createImageUrl(buffer, type);
+    setTextureUrl(imageUrl);
+  };
   return (
-    <>
-      <Canvas
-        dpr={[1, 2]}
-        camera={{
-          fov: 45,
-          position: [
-            -2.1089695006544007, 0.06982244031026139, 4.53292096469145,
-          ],
-          rotation: [
-            -0.015402191985851415, -0.4354229232915067, -0.006496972981844338,
-          ],
-        }}
-      >
-        <Scene setSofa={smallSofa} toggleSofa={smallSofa} />
-        {/* <Perf position="top-left" overClock /> */}
+    <AppContext.Provider value={{ textureUrl }}>
+      <Canvas dpr={[1, 2]}>
+        <Stage adjustCamera preset="rembrandt" intensity={0.6}>
+          <Suspense fallback={null}>
+            <Headboard />
+          </Suspense>
+        </Stage>
+        <Controls />
       </Canvas>
-      <Overlay setSmallSofa={setSmallSofa} />
+      <Overlay getTextureFile={getTextureFile} />
       <Loader />
-    </>
+    </AppContext.Provider>
+  );
+}
+
+function Headboard() {
+  const { scene } = useGLTF("/Madison_rechte_6cm.glb");
+  const mesh = useRef();
+  const material = useRef();
+  const { textureUrl } = useContext(AppContext);
+  useEffect(() => {
+    if (textureUrl) {
+      const textureLoader = new TextureLoader();
+      textureLoader.load(textureUrl, (texture) => {
+        texture.encoding = sRGBEncoding;
+        texture.flipY = false;
+        texture.repeat.set(5, 5);
+        texture.wrapS = RepeatWrapping;
+        texture.wrapT = RepeatWrapping;
+
+        const model = scene.children[0];
+        if (model.material.map) {
+          model.material.map.dispose();
+        }
+        model.material.map = texture;
+        model.material.needsUpdate = true;
+      });
+    }
+  }, [textureUrl]);
+  return <primitive object={scene}></primitive>;
+}
+
+function Overlay({ getTextureFile }) {
+  return (
+    <ContainerOverlay>
+      <Text>Upload your texture</Text>
+      <input type="file" onChange={(e) => getTextureFile(e)} />
+    </ContainerOverlay>
   );
 }
 
 export default App;
+
+const ContainerOverlay = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+  width: 40vw;
+  height: 6vh;
+  bottom: 2vh;
+  right: 2vw;
+  border-radius: 10px;
+  background: rgba(158, 190, 230, 0.608);
+  box-shadow: 0 8px 32px 0 rgba(255, 255, 255, 0.17);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+`;
+
+const Text = styled.p`
+  color: #fff;
+  font-weight: 100;
+  font-size: 0.9rem;
+`;
+
+useGLTF.preload("/Madison_rechte_6cm.glb");
--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,22 +1,15 @@
 import { Suspense } from "react";
 import Controls from "./Controls";
 import { Environment } from "@react-three/drei";
-import { Room } from "./Room";
-import Effects from "./Effects";
-import Sofa from "./Sofa";
-import Sofa2 from "./Sofa2";
 
 // eslint-disable-next-line react/prop-types
-function Scene({ setSofa, toggleSofa }) {
+function Scene() {
   return (
     <>
       <Controls />
       <Suspense fallback={null}>
-        <Room toggleSofa={toggleSofa} />
-        <Environment files={"/pine_attic_1k.hdr"} background blur={0.4} />
+        <Environment files={"/pine_attic_1k.hdr"} />
       </Suspense>
-      {setSofa ? <Sofa /> : <Sofa2 />}
-      <Effects />
     </>
   );
 }
